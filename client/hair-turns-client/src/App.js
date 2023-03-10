6cb0bb04d3f7e43b79a45a225bc544540ff1540f
romerojulian1700 <romerojulian1700@gmail.com>
2023-03-10 20:22:04 -0300

    Configurando React y practicando Juli

--- client/hair-turns-client/src/App.js
+++ client/hair-turns-client/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
+import logo1 from './logo1.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Bienvenidos</h2>
         <h1>Hair Turns client</h1>
+        <img src={logo1} className="App-logo" alt="logo" />
+
+
+        <button className="btn">Ingresa</button>
       </header>
     </div>
   );
